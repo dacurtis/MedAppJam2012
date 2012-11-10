a6b68631ac7839488f9a10ff9c3c10082190be7e
dacurtis <dc8curtis@gmail.com>
2012-11-10 13:54:09 -0800

    Test

---
+++ Test/TestTests/TestTests.h
@@ -0,0 +1,13 @@
+//
+//  TestTests.h
+//  TestTests
+//
+//  Created by App Jam on 11/10/12.
+//  Copyright (c) 2012 App Jam. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestTests : SenTestCase
+
+@end
---
+++ Test/TestTests/TestTests.m
@@ -0,0 +1,32 @@
+//
+//  TestTests.m
+//  TestTests
+//
+//  Created by App Jam on 11/10/12.
+//  Copyright (c) 2012 App Jam. All rights reserved.
+//
+
+#import "TestTests.h"
+
+@implementation TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestTests");
+}
+
+@end
