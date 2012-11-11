c5b5473b9067b367c6d89181de1796e478dffe71
Matt <mgardner091@gmail.com>
2012-11-11 15:02:59 -0800

    IncidentReport Xcode Project

---
+++ IncidentReport/IncidentReport/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IncidentReport
+//
+//  Created by Matt Gardner on 11/11/12.
+//  Copyright (c) 2012 MedAppJam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
