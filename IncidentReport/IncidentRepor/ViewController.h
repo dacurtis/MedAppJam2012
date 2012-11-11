93ca1a09cbff3e8ddc73a8f4abd32bd45c255054
Matt <mgardner091@gmail.com>
2012-11-11 14:49:39 -0800

    Incident Report Xcode Project

---
+++ IncidentReport/IncidentRepor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IncidentRepor
+//
+//  Created by Matt Gardner on 11/11/12.
+//  Copyright (c) 2012 MedAppJam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ IncidentReport/IncidentReporTests/IncidentReporTests.h
@@ -0,0 +1,13 @@
+//
+//  IncidentReporTests.h
+//  IncidentReporTests
+//
+//  Created by Matt Gardner on 11/11/12.
+//  Copyright (c) 2012 MedAppJam. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IncidentReporTests : SenTestCase
+
+@end
