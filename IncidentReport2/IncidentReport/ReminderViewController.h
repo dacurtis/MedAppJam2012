3f5c9d9e5df8531ec40f2842bbe639f2d9a7c315
dacurtis <dc8curtis@gmail.com>
2012-11-13 13:41:25 -0800

    Adds a few new half-implemented features

---
+++ IncidentReport2/IncidentReport/ReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderViewController.h
+//  IncidentReport
+//
+//  Created by App Jam on 11/13/12.
+//  Copyright (c) 2012 MedAppJam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReminderViewController : UIViewController
+
+@end
